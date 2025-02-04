85e1d967d18eb545d4d9d9b72bfcaf3592c042ad
{taku-kappa} <{kamijimatakuya15@gmail.com}>
2025-02-04 17:51:31 +0900

    4章 オブジェクト指向を使いこなそう(4-5 演習問題)追加

---
+++ java_youtube/4-5/Car2.java
@@ -0,0 +1,31 @@
+public class Car2 {
+    private int no;
+
+    public Car2(int no) {
+        this.no = no;
+    }
+
+    public void display() {
+        System.out.println("ナンバーは" + no + "です");
+    }
+    
+}
+
+
+// Car c = new Car();
+// Taxi t1 = new Taxi(2525);
+// Taxi t0 = new Taxi();
+// Taxi t2 = new Taxi("2525");
+
+
+
+// Car()
+
+// Car(int no)
+// Taxi(int no)
+
+// Car(int no)
+// Taxi(int no)
+// Taxi()
+
+// Taxi(String no)
---
+++ java_youtube/4-5/Taxi2.java
@@ -0,0 +1,27 @@
+public class Taxi2 extends Car2 {
+    private int price;
+
+    public Taxi2(int no) {
+        super(no);
+        this.price = 0;
+    }
+
+    public Taxi2 (int no, int price) {
+        super(no);
+        this.price = price;
+    }
+
+    public void start() {
+        price = 420;
+    }
+
+    public void run() {
+        price += 80;
+    }
+    
+    public void display() {
+        super.display();
+        System.out.println("料金は" + price + "円です");
+    }
+        
+}
