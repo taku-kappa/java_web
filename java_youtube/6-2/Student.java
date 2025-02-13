20e33c8cc609973a6862ea1c3460ae409d3893e6
{taku-kappa} <{kamijimatakuya15@gmail.com}>
2025-02-13 18:14:24 +0900

    6章 クラスライブラリを使ってみよう(6-2)追加

---
+++ java_youtube/6-2/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "私の名前は" + name + "です";
+    }
+    
+}
